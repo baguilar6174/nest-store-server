9b9284a90e9da376bdef3c1308b39443b869620a
Bryan Aguilar <bryan.aguilar6174@gmail.com>
2022-11-25 22:48:50 -0500

    feat: Create products resource (create product in DB), and common filters and exception for http request

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { HttpExceptionFilter } from './common/filters/http-exception/http-exception.filter';
+import { TimeoutInterceptor } from './common/interceptors/timeout/timeout.interceptor';
 
-async function bootstrap() {
+async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.setGlobalPrefix('api/v1');
+  app.useGlobalFilters(new HttpExceptionFilter());
+  app.useGlobalInterceptors(new TimeoutInterceptor());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
   await app.listen(3000);
 }
 bootstrap();
---
+++ src/modules/products/dto/create-product.dto.ts
@@ -0,0 +1,44 @@
+import {
+  ArrayNotEmpty,
+  IsInt,
+  IsNotEmpty,
+  IsNumber,
+  IsString,
+  IsArray,
+  IsPositive,
+  IsOptional,
+  IsIn,
+} from 'class-validator';
+
+export class CreateProductDto {
+  @IsString()
+  @IsNotEmpty()
+  title: string;
+
+  @IsNumber()
+  @IsPositive()
+  @IsOptional()
+  price?: number;
+
+  @IsString()
+  @IsOptional()
+  description?: string;
+
+  @IsString()
+  @IsOptional()
+  slug?: string;
+
+  @IsInt()
+  @IsPositive()
+  @IsOptional()
+  stock?: number;
+
+  @IsString({ each: true })
+  @IsArray()
+  @ArrayNotEmpty({ message: `At least one size` })
+  sizes: string[];
+
+  @IsString()
+  @IsIn(['men', 'women', 'kid', 'unisex'])
+  gender: string;
+}
