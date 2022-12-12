e2cde61791b934509138fd6eb65dd4c6efa93544
Bryan Aguilar <bryan.aguilar6174@gmail.com>
2022-12-12 16:26:11 -0500

    feat: Add sigin endpoint with validations

--- src/modules/auth/auth.controller.ts
+++ src/modules/auth/auth.controller.ts
@@ -1,7 +1,7 @@
 import { Controller, Post, Body } from '@nestjs/common';
 
 import { AuthService } from './auth.service';
-import { CreateUserDto } from './dto/create-user.dto';
+import { CreateUserDto, LoginUserDto } from './dto';
 import { User } from './entities/user.entity';
 
 @Controller('auth')
@@ -9,7 +9,12 @@ export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('signup')
-  async signUp(@Body() createUserDto: CreateUserDto): Promise<User> {
+  async signup(@Body() createUserDto: CreateUserDto): Promise<User> {
     return await this.authService.signUp(createUserDto);
+  }
+
+  @Post('signin')
+  async signin(@Body() loginUserDto: LoginUserDto): Promise<User> {
+    return await this.authService.signin(loginUserDto);
   }
 }
--- src/modules/auth/auth.service.ts
+++ src/modules/auth/auth.service.ts
@@ -1,9 +1,9 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Repository } from 'typeorm';
 import * as bcrypt from 'bcrypt';
 
-import { CreateUserDto } from './dto/create-user.dto';
+import { CreateUserDto, LoginUserDto } from './dto';
 import { User } from './entities/user.entity';
 
 @Injectable()
@@ -25,8 +25,27 @@ export class AuthService {
     return user;
   }
 
+  async signin(loginUserDto: LoginUserDto): Promise<User> {
+    const { password, email } = loginUserDto;
+    const user = await this.userRepository.findOne({
+      where: { email },
+      select: { email: true, password: true },
+    });
+    if (!user) throw new UnauthorizedException(`Credentials are not valid`);
+    const isValidPassword = await this.checkPassword(password, user.password);
+    if (!isValidPassword) {
+      throw new UnauthorizedException(`Credentials are not valid`);
+    }
+    // TODO: include JWT
+    return user;
+  }
+
   async hashPassword(password: string): Promise<string> {
     const salt = await bcrypt.genSalt(10);
     return await bcrypt.hash(password, salt);
+  }
+
+  async checkPassword(password: string, passwordDB: string): Promise<boolean> {
+    return await bcrypt.compare(password, passwordDB);
   }
 }
